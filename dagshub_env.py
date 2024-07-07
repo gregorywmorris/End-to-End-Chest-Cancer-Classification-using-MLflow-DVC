034efb4865759c6bee674f6ac8b2f51ac28d43bd
gregorywmorris <gwm1980@gmail.com>
2024-07-07 09:21:34 -0400

    dagsub connected

--- dagshub_env.py
+++ dagshub_env.py
@@ -1,7 +1,8 @@
 import dagshub
+import mlflow
+
 dagshub.init(repo_owner='gregorywmorris', repo_name='End-to-End-Chest-Cancer-Classification-using-MLflow-DVC', mlflow=True)
 
-import mlflow
 with mlflow.start_run():
   mlflow.log_param('parameter name', 'value')
   mlflow.log_metric('metric name', 1)
